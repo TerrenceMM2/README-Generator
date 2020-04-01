41171ee039ee2e175056ead90e4ea6fea5836429
Terrence Mahnken <terrencemm2@gmail.com>
2020-04-01 15:49:45 -0500

    adds additional inquirer questions

--- index.js
+++ index.js
@@ -8,12 +8,72 @@ const userInput = async () => {
             type: "input",
             message: "What is your GitHub username?",
             name: "githubUsername"
-        }
+        },
+        {
+            type: "input",
+            message: "Project Title?",
+            name: "projectTitle"
+        },
+        {
+            type: "input",
+            message: "Project Description?",
+            name: "projectDescription"
+        },
+        {
+            type: "input",
+            message: "Please provide a Table of Contents",
+            name: "tableOfContents",
+            default: "Installation, Usage, Credits, License"
+        },
+        {
+            type: "input",
+            message: "Installation Instructions",
+            name: "installation",
+            default: "npm i"
+        },
+        {
+            type: "input",
+            message: "Usage Information",
+            name: "usage"
+        },
+        {
+            type: "list",
+            message: "License",
+            name: "license",
+            choices: ["MIT", "Apache", "ISC", "GNU", "None"]
+        },
+        {
+            type: "input",
+            message: "Contribution Information",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "Testing Information",
+            name: "testing",
+            default: "npm test"
+        },
+        {
+            type: "input",
+            message: "Question Information",
+            name: "questions"
+        },
     ]);
 
     const confirm = await inquirer.prompt([{
         type: "confirm",
-        message: `GitHub Username: ${data.githubUsername}\nIs this correct?`,
+        message: 
+`GitHub Username: ${data.githubUsername}
+Project Title: ${data.githubUsername}
+Project Description: ${data.projectDescription}
+Table of Contents: ${data.tableOfContents}
+Installation: ${data.installation}
+Usage: ${data.usage}
+License: ${data.license}
+Contribution: ${data.contribution}
+Testing: ${data.testing}
+Questions: ${data.questions}
+Is this correct?`,
         name: "confirmed",
     }]);
 
